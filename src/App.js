05c6fe13b5d533fd687ac20687a2f6465005bdfc
Nathan Merrill <nmerrill5@gmail.com>
2022-05-25 07:06:57 -0600

    Displays list of todos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return <div>Hello world</div>;
+  const [todos, setTodos] = useState([{ text: 'Pick up grandma' }, { text: 'Go to store' }]);
+
+  return (
+    <div className="todo-container">
+      <h3>Welcome to the todos app!</h3>
+      {todos.map((todo, index) => (
+        <div>{todo.text}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
