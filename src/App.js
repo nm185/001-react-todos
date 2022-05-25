6575d37c5a5b1377f59d4aec8543531dae505153
Nathan Merrill <nmerrill5@gmail.com>
2022-05-25 08:34:13 -0600

    Added UI to add todo items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import { useState } from 'react';
 import Todo from './components/todo';
+import NewTodoInput from './components/new_todo';
 
 function App() {
   const [todos, setTodos] = useState([{ text: 'Pick up grandma' }, { text: 'Go to store' }]);
+
+  const addTodo = (text) => {
+    if (!text || text.length === 0) return;
+    setTodos([...todos, { text }]);
+  };
 
   return (
     <div className="todo-container">
@@ -11,6 +17,7 @@ function App() {
       {todos.map((todo, index) => (
         <Todo key={index} todo={todo} />
       ))}
+      <NewTodoInput addTodo={addTodo} />
     </div>
   );
 }
