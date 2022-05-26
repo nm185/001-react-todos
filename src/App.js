292dbbd3b935f66413237b88a8ea05459aebe752
Nathan Merrill <nmerrill5@gmail.com>
2022-05-26 06:03:40 -0600

    Toggle todo working

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import Todo from './components/todo';
 import NewTodoInput from './components/new_todo';
 
 function App() {
-  const [todos, setTodos] = useState([{ text: 'Pick up grandma' }, { text: 'Go to store' }]);
+  const [todos, setTodos] = useState([
+    { text: 'Pick up grandma', checked: false },
+    { text: 'Go to store', checked: true },
+  ]);
 
   const addTodo = (text) => {
     if (!text || text.length === 0) return;
-    setTodos([...todos, { text }]);
+    setTodos([...todos, { text, checked: false }]);
+  };
+
+  const setCheckedState = (index, checked) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].checked = checked;
+    setTodos(updatedTodos);
   };
 
   return (
     <div className="todo-container">
       <h3>Welcome to the todos app!</h3>
       {todos.map((todo, index) => (
-        <Todo key={index} todo={todo} />
+        <Todo key={index} todo={todo} index={index} setCheckedState={setCheckedState} />
       ))}
       <NewTodoInput addTodo={addTodo} />
     </div>
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,8 +1,9 @@
-const Todo = ({ todo }) => {
+const Todo = ({ todo, index, setCheckedState }) => {
   return (
-    <>
-      <div>{todo.text}</div>
-    </>
+    <div>
+      <input type="checkbox" checked={todo.checked} onChange={() => setCheckedState(index, !todo.checked)} />
+      <span>{todo.text}</span>
+    </div>
   );
 };
 
