bcc2e05c250bb694edf952395b80cfb569c18949
Nathan Merrill <nmerrill5@gmail.com>
2022-05-26 06:13:27 -0600

    Moved child components to parent file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import './App.css';
 import { useState } from 'react';
-import Todo from './components/todo';
-import NewTodoInput from './components/new_todo';
+
+const Todo = ({ todo, index, setCheckedState }) => {
+  return (
+    <div>
+      <input type="checkbox" checked={todo.checked} onChange={() => setCheckedState(index, !todo.checked)} />
+      <span>{todo.text}</span>
+    </div>
+  );
+};
+
+const NewTodoInput = ({ addTodo }) => {
+  const [todoText, setTodoText] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTodo(todoText);
+    setTodoText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="new-todo">New Todo</label>
+        <input
+          id="new-todo"
+          type="text"
+          value={todoText}
+          onChange={(e) => {
+            setTodoText(e.target.value);
+          }}
+        ></input>
+      </div>
+    </form>
+  );
+};
 
 function App() {
   const [todos, setTodos] = useState([
@@ -20,11 +53,16 @@ function App() {
     setTodos(updatedTodos);
   };
 
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="todo-container">
       <h3>Welcome to the todos app!</h3>
       {todos.map((todo, index) => (
-        <Todo key={index} todo={todo} index={index} setCheckedState={setCheckedState} />
+        <Todo key={index} todo={todo} index={index} setCheckedState={setCheckedState} delete={deleteTodo} />
       ))}
       <NewTodoInput addTodo={addTodo} />
     </div>
